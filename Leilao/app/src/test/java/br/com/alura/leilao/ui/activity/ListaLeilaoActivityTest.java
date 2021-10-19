c8e5505fe79dd962041f91dba54b22ae512ac309
Carlos M Valenca <cvalenca@everis.com>
2021-10-19 17:18:35 -0300

    Mockando as dependencias do teste

--- Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
+++ Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/ListaLeilaoActivityTest.java
@@ -3,18 +3,33 @@ package br.com.alura.leilao.ui.activity;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import android.content.Context;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoActivityTest {
+
+    @Mock
+    private Context context;
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizarListaDeLeiloes_QuandoBuscarLeiloesDaApi() throws InterruptedException {
         ListaLeilaoActivity activity = new ListaLeilaoActivity();
+        Mockito.doNothing().when(adapter).atualizaLista();
 
-        activity.configuraAdapter();
-        activity.buscaLeiloes();
+        activity.buscaLeiloes(adapter);
         Thread.sleep(2000);
-        int quantidadeLeiloesDevolvida = activity.getAdapter().getItemCount();
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
 
         assertThat(quantidadeLeiloesDevolvida, is(3));
     }
